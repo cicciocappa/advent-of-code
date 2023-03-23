982c3a2a4ea59b0c8311f681a26b74e1955d7624
cicciocappa <49641638+cicciocappa@users.noreply.github.com>
2023-03-23 09:02:41 +0100

    Update day-20.js
    correct solution

--- 2016/day-20.js
+++ 2016/day-20.js
@@ -1,32 +1,22 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('./day-20-input.txt', 'utf8').trimEnd(),
-  max = 4294967295;
+const ranges = input.split('\n').map(line => line.split('-').map(i => parseInt(i, 10)));
+const MAX = 4294967295;
 
-function solve(input) {
-  const blacklist = input
-    .split('\n')
-    .map((line) => line.split('-').map(Number))
-    .sort((a, b) => a[0] - b[0]);
+ranges.sort((a, b) => a[0] - b[0]);
 
-  const filtered = [];
-  for (const curr of blacklist) {
-    const prev = filtered[filtered.length - 1];
-    if (prev && curr[0] < prev[1]) {
-      prev[1] = Math.max(prev[1], curr[1]);
-    } else {
-      filtered.push(curr);
-    }
-  }
-  console.log(filtered[0][1] + 1);
+let allowedCount = 0;
+let lastMax = -1;
+let firstAllowed;
 
-  let count = filtered[0][0];
-  for (let i = 1; i < filtered.length; i++) {
-    const prev = filtered[i - 1];
-    const curr = filtered[i];
-    count += curr[0] - prev[1] - 1;
-  }
-  count += max - filtered[filtered.length - 1][1];
-  console.log(count);
+for (let i = 0; i < ranges.length; i++) {
+  const c = Math.max(0, ranges[i][0] - lastMax - 1);
+  allowedCount += c;
+  if (firstAllowed === undefined && c) firstAllowed = lastMax + 1;
+  lastMax = Math.max(lastMax, ranges[i][1]);
 }
-solve(input);
+allowedCount += Math.max(0, MAX - lastMax);
+
+console.log('Part 1', firstAllowed);
+console.log('Part 2', allowedCount);
